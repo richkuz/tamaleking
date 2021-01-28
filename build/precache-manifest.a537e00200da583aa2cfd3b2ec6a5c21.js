5e62b9fe1b82bd2adc7913697632844f5ef6706c
Rich Kuzsma <62522248+richkuz@users.noreply.github.com>
2021-01-28 11:35:16 -0500

    Built artifact

---
+++ build/precache-manifest.a537e00200da583aa2cfd3b2ec6a5c21.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c693d231ac5c562259985eebda19bb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7561ed46f50607a726a",
+    "url": "/static/css/main.8dab7194.css"
+  },
+  {
+    "revision": "f7561ed46f50607a726a",
+    "url": "/static/js/main.116263b0.js"
+  },
+  {
+    "revision": "95ea6e06508ae6cb347ab110e0d2cd19",
+    "url": "/static/js/main.116263b0.js.LICENSE.txt"
+  }
+]);
